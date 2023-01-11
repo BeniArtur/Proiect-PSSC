1c83764160fa67c396135c4db31505aad83460bd
BeniArtur <92297415+BeniArtur@users.noreply.github.com>
2023-01-11 17:37:36 +0200

    Add files via upload

---
+++ Package.cs
@@ -0,0 +1,44 @@
+﻿using LanguageExt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleApp1.Data.Payment;
+using static ConsoleApp1.Data.Payment.PriceFinal;
+
+namespace ConsoleApp1.Data
+{
+    internal class Package
+    {
+        public record Product(int Id, Quantity Quantity, PriceFinal PriceFinal,DateTime Date);
+        public record Delivery
+        {
+            private int id;
+            private Product[] products;
+            private string address;
+            private DateTime Date;
+            public Delivery(int id, Product[] products)
+            {
+                this.id = id;
+                this.products = products;
+            }
+
+            private Delivery(int id, Product[] products, string address, DateTime Date)
+            {
+                this.address= address;
+                this.Date = Date;
+            }
+
+            public static Option<Delivery> Create(int id, Product[] products, string address, DateTime Date) =>
+                Some(new Delivery(id, products, address,Date));
+
+            private static Option<Delivery> Some(Delivery delivery)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+}
+
---
+++ Payment.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LanguageExt;
+using PayPal.Api;
+using static LanguageExt.Prelude;
+
+namespace ConsoleApp1.Data
+{
+    public class Payment
+    {
+        public record Product(int Id, Quantity Quantity, PriceFinal PriceFinal, uint Price, DateTime Date);
+
+        public record PriceFinal
+        {
+            public uint Final { get; }
+
+            private PriceFinal(uint final)
+            {
+                Final = final;
+
+            }
+        }
+            public record Invoice
+            {
+                private int id;
+                private Product[] products;
+                private double Total;
+            private DateTime Date;
+
+                private Invoice(int id, Product[] products)
+                {
+                    this.id = id;
+                    this.products = products;
+                }
+
+                private Invoice(int id, Product[] products,double Total, DateTime Date)
+                {
+                    this.Total = Total;
+                this.Date = Date;
+                }
+
+                public static Option<Invoice> Create(int id, Product[] products,double Total, DateTime Date) =>
+                    Some(new Invoice(id, products,Total,Date));
+
+            internal static object Create(int v1, Product[] products, int v2)
+            {
+                throw new NotImplementedException();
+            }
+
+            internal static object Create(int v1, Product[] products, int v2, int v3, int v4, int v5)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        }
+    }
+
